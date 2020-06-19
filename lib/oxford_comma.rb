911a6e3f79007abac9a06fbebd2e08c44a692a01
Jessica Pereira <jcpereira070@gmail.com>
2020-06-19 02:45:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(fruits)
   if fruits.size == 1
   return fruits [0]
-end 
-fruits. join (" and ")
-end
+elsif fruits.size == 2
+return fruits. join (" and ")
+else
+    return fruits(..-2)
